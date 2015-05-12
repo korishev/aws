b9fa868c367e036364504cb1ffeddc4e1fa07c3b
Morgan Nelson <mnelson@steele-forge.com>
2015-05-12 14:52:07 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ description       "LWRPs for managing AWS resources"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.100.0"
 recipe            "aws", "Installs the right_aws gem during compile time"
+name              "aws"
